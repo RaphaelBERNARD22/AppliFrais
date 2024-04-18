cfadac35f405ab810ba2bd264f79841dd547cece
Anais BODART <anais.bodart@lycee-basch.fr>
2024-04-18 17:54:23 +0200

    mdp db hash et comments

--- hash.php
+++ hash.php
@@ -1,8 +1,7 @@
 <?php
 require_once ("www/include/class.pdogsb.inc.php");
-
 $config = include('config.php');
-
+//se connecte à la base de donées.
 $chaineConnexion = 'mysql:host=' . $config["host"] . ';dbname=' . $config["database"];
 // demande que le dialogue se fasee en utilisant l'encodage utf-8
 // et le mode de gestion des erreurs soit les exceptions
@@ -10,12 +9,13 @@ $params = array (   PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 // crée une instance de PDO (connexion avec le serveur MySql) 
 $monPdo = new PDO($chaineConnexion,  $config["user"], $config["password"], $params); 
-
+//prépare la requête sql.
 $query = "SELECT id, mdp FROM Visiteur";
 $cmd = $monPdo -> prepare($query);
+//execute.
 $cmd->execute();
 $lignes = $cmd->fetchAll();
-
+ //hash tous les mots de passes, id par id, à l'aide de bcrypt, en updatant la bdd gsbfrais.
 foreach($lignes as $ligne){
  $query ='UPDATE Visiteur SET `mdp`=? WHERE id=?';
  $cmd = $monPdo->prepare($query);
@@ -24,8 +24,5 @@ foreach($lignes as $ligne){
  $cmd->bindValue(2, $ligne['id']);
  $cmd->execute();
 }
-
-
-//$monPdo->commit();
 
 
